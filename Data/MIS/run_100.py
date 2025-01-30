d1e824f6d39f319747273878395e8c6db49fdeb6
Alejandro Montanez <alejomonbar@gmail.com>
2025-01-30 09:53:30 +0100

    adding hpc cases

---
+++ Data/MIS/run_100.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import ast
+
+nq = sys.eval[1]
+ps = ast.literlal_eval(sys.eval[2])
+gammas = ast.literlal_eval(sys.eval[3])
+betas = ast.literal_eval(sys.eval[4])
+cases = ast.litelal_eval(sys.eval[5])
+
+for p in ps:
+	for case in cases:
+		delta_beta = betas[p]
+		delta_gamma = gammas[p]
+		
+
+
+
